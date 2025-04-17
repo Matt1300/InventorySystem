231d3e1abadb1ede2d2f60648bc08a5b853ae90e
Matt1300 <mateogranizo1304@gmail.com>
2025-04-16 23:28:31 -0500

    Integrate Swagger and improve DI and launch settings

--- InventorySystem.Api/Program.cs
+++ InventorySystem.Api/Program.cs
@@ -1,3 +1,6 @@
+using InventorySystem.Application.Implementations;
+using InventorySystem.Application.Interfaces;
+using InventorySystem.Application.Interfaces.Repositories;
 using InventorySystem.Application.Interfaces.UnitOfWork;
 using InventorySystem.Infrastructure.Contexts;
 using InventorySystem.Infrastructure.Repositories;
@@ -12,15 +15,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddAutoMapper(typeof(InventorySystem.Application.Mappings.AutoMapperProfiles));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
